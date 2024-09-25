e382316e97b7aea57c8b992a2bdf9cd26113e116
Avinash Yadav <95971529+yavinash249@users.noreply.github.com>
2024-09-25 13:30:02 +0530

    change in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('hey this is me avinash aksavinash'));
+app.get('/', (req, res) => res.send('hey this koi job dil wado bhia   avinash'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
